77110709562d64ab484fbbac69ebf7e008ae3647
Bianca <bianca.darolti@softwire.com>
2022-09-30 16:05:42 +0100

    Passed Red-11 (Bishop cannot move when blocked)

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -12,17 +12,18 @@ export default class Bishop extends Piece {
     public getAvailableMoves(board: Board) {
         let possibleMoves: Square[] = [];
         let piecePosition: Square = board.findPiece(this);
-        // When moving diagonally, the difference and sum of the row and column remain the same
-        let delta = piecePosition.row - piecePosition.col;
-        let sum = piecePosition.row + piecePosition.col;
-        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) {
-            if (i !== piecePosition.col) {
-                let Pos1 = new Square(i + delta, i);
-                if (Pos1.checkInRange())
+        // Stores if each diagonal has been blocked yet
+        let unBlocked: boolean[] = [true, true, true, true];
+        for (let i = 1; i < GameSettings.BOARD_SIZE; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (unBlocked[j]) {
+                    let Pos1 = new Square(piecePosition.row + i * (2 * (j % 2) - 1), piecePosition.col + i * (2 * Math.floor(j / 2) - 1));
+                    if (!Pos1.checkInRange() || board.getPiece(Pos1) !== undefined) {
+                        unBlocked[j] = false;
+                        continue;
+                    }
                     possibleMoves.push(Pos1);
-                let Pos2 = new Square(sum - i, i);
-                if (Pos2.checkInRange())
-                    possibleMoves.push(Pos2);
+                }
             }
         }
         return possibleMoves;
