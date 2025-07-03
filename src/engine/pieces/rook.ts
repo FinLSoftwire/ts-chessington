8602c690c022511e490b7ddc7621bfb73fe5b7a6
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 12:18:26 +0100

    Started Red-13 (Rook cannot take same player's pieces)

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -15,32 +15,44 @@ export default class Rook extends Piece {
         let rowIndex = piecePosition.row-1;
         while (rowIndex >= 0) {
             let newMove = new Square(rowIndex, piecePosition.col);
-            possibleMoves.push(newMove);
-            if (typeof board.getPiece(newMove) !== 'undefined')
+            if (typeof board.getPiece(newMove) !== 'undefined') {
+                if (board.getPiece(newMove)?.player !== this.player)
+                    possibleMoves.push(newMove);
                 break;
+            }
+            possibleMoves.push(newMove);
             rowIndex--;
         }
         let colIndex = piecePosition.col-1;
         while (colIndex >= 0) {
             let newMove = new Square(piecePosition.row, colIndex);
-            possibleMoves.push(newMove);
-            if (typeof board.getPiece(newMove) !== 'undefined')
+            if (typeof board.getPiece(newMove) !== 'undefined') {
+                if (board.getPiece(newMove)?.player !== this.player)
+                    possibleMoves.push(newMove);
                 break;
+            }
+            possibleMoves.push(newMove);
             colIndex--;
         }
         rowIndex = piecePosition.row+1;
         while (rowIndex < GameSettings.BOARD_SIZE) {
             let newMove = new Square(rowIndex, piecePosition.col);
-            possibleMoves.push(newMove);
-            if (typeof board.getPiece(newMove) !== 'undefined')
+            if (typeof board.getPiece(newMove) !== 'undefined') {
+                if (board.getPiece(newMove)?.player !== this.player)
+                    possibleMoves.push(newMove);
                 break;
+            }
+            possibleMoves.push(newMove);
             rowIndex++;
         }
         colIndex = piecePosition.col+1;
         while (colIndex < GameSettings.BOARD_SIZE) {
             let newMove = new Square(piecePosition.row, colIndex);
-            if (typeof board.getPiece(newMove) !== 'undefined')
+            if (typeof board.getPiece(newMove) !== 'undefined') {
+                if (board.getPiece(newMove)?.player !== this.player)
+                    possibleMoves.push(newMove);
                 break;
+            }
             possibleMoves.push(newMove);
             colIndex++;
         }
