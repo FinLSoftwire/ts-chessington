2f0e8766e8290401dd96640235092de371fd6d18
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 12:16:07 +0100

    Started Red-13 (Rook can take pieces)

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -15,25 +15,25 @@ export default class Rook extends Piece {
         let rowIndex = piecePosition.row-1;
         while (rowIndex >= 0) {
             let newMove = new Square(rowIndex, piecePosition.col);
+            possibleMoves.push(newMove);
             if (typeof board.getPiece(newMove) !== 'undefined')
                 break;
-            possibleMoves.push(newMove);
             rowIndex--;
         }
         let colIndex = piecePosition.col-1;
         while (colIndex >= 0) {
             let newMove = new Square(piecePosition.row, colIndex);
+            possibleMoves.push(newMove);
             if (typeof board.getPiece(newMove) !== 'undefined')
                 break;
-            possibleMoves.push(newMove);
             colIndex--;
         }
         rowIndex = piecePosition.row+1;
         while (rowIndex < GameSettings.BOARD_SIZE) {
             let newMove = new Square(rowIndex, piecePosition.col);
+            possibleMoves.push(newMove);
             if (typeof board.getPiece(newMove) !== 'undefined')
                 break;
-            possibleMoves.push(newMove);
             rowIndex++;
         }
         colIndex = piecePosition.col+1;
