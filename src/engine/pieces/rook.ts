08256d929e484db7d87e348c8c54a79734aa4875
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 12:30:07 +0100

    Refactored Rook code to be cleaner

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -10,52 +10,32 @@ export default class Rook extends Piece {
         super(player);
     }
 
-    public getAvailableMoves(board: Board) {
-        let possibleMoves: Square[] = [];
+    public getAvailableMoves(board: Board) {let possibleMoves: Square[] = [];
         let piecePosition: Square = board.findPiece(this);
-        let rowIndex = piecePosition.row-1;
-        while (rowIndex >= 0) {
-            let newMove = new Square(rowIndex, piecePosition.col);
-            if (typeof board.getPiece(newMove) !== 'undefined') {
-                if (board.getPiece(newMove)?.player !== this.player && !(board.getPiece(newMove) instanceof King))
-                    possibleMoves.push(newMove);
-                break;
+        // Stores if each diagonal has been blocked yet
+        let unBlocked: boolean[] = [true, true, true, true];
+        for (let i = 1; i < GameSettings.BOARD_SIZE; i++) {
+            for (let j = 0; j < 4; j++) {
+                if (unBlocked[j]) {
+                    let Pos1;
+                    if (j % 2 === 0) {
+                        Pos1 = new Square(piecePosition.row, piecePosition.col + i * (2 * Math.floor(j / 2) - 1));
+                    } else {
+                        Pos1 = new Square(piecePosition.row + i * (2 * Math.floor(j / 2) - 1), piecePosition.col);
+                    }
+                    if (!Pos1.checkInRange()) {
+                        unBlocked[j] = false;
+                        continue;
+                    }
+                    if (typeof board.getPiece(Pos1) !== 'undefined') {
+                        if (board.getPiece(Pos1)?.player !== this.player && !(board.getPiece(Pos1) instanceof King))
+                            possibleMoves.push(Pos1);
+                        unBlocked[j] = false;
+                        continue;
+                    }
+                    possibleMoves.push(Pos1);
+                }
             }
-            possibleMoves.push(newMove);
-            rowIndex--;
-        }
-        let colIndex = piecePosition.col-1;
-        while (colIndex >= 0) {
-            let newMove = new Square(piecePosition.row, colIndex);
-            if (typeof board.getPiece(newMove) !== 'undefined') {
-                if (board.getPiece(newMove)?.player !== this.player && !(board.getPiece(newMove) instanceof King))
-                    possibleMoves.push(newMove);
-                break;
-            }
-            possibleMoves.push(newMove);
-            colIndex--;
-        }
-        rowIndex = piecePosition.row+1;
-        while (rowIndex < GameSettings.BOARD_SIZE) {
-            let newMove = new Square(rowIndex, piecePosition.col);
-            if (typeof board.getPiece(newMove) !== 'undefined') {
-                if (board.getPiece(newMove)?.player !== this.player && !(board.getPiece(newMove) instanceof King))
-                    possibleMoves.push(newMove);
-                break;
-            }
-            possibleMoves.push(newMove);
-            rowIndex++;
-        }
-        colIndex = piecePosition.col+1;
-        while (colIndex < GameSettings.BOARD_SIZE) {
-            let newMove = new Square(piecePosition.row, colIndex);
-            if (typeof board.getPiece(newMove) !== 'undefined') {
-                if (board.getPiece(newMove)?.player !== this.player && !(board.getPiece(newMove) instanceof King))
-                    possibleMoves.push(newMove);
-                break;
-            }
-            possibleMoves.push(newMove);
-            colIndex++;
         }
         return possibleMoves;
     }
