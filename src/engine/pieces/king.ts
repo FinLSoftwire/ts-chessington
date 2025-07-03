e66146198e3c7c156303ba808bd503ec8caa32a6
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 10:29:55 +0100

    Passed Test Red-7 (King only moves to all adjacent squares)

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,18 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let possibleMoves: Square[] = [];
+        let piecePosition: Square = board.findPiece(this);
+        for (let rowDelta = -1; rowDelta < 2; rowDelta++) {
+            for (let colDelta = -1; colDelta < 2; colDelta++) {
+                if (colDelta === 0 && rowDelta === 0)
+                    continue;
+                let newPos = new Square(piecePosition.row + rowDelta, piecePosition.col + colDelta);
+                if (newPos.col >= 0 && newPos.col < GameSettings.BOARD_SIZE && newPos.row >= 0 && newPos.row < GameSettings.BOARD_SIZE) {
+                    possibleMoves.push(newPos);
+                }
+            }
+        }
+        return possibleMoves;
     }
 }
