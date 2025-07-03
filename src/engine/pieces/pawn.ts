b1de21c2c4d6bb74b4cd0274584896580c2404ba
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 12:14:19 +0100

    Passed Red-12 (Ensures pawns cannot move off of the board)

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -20,12 +20,12 @@ export default class Pawn extends Piece {
         let piecePosition: Square = board.findPiece(this);
         let playerDirection: number = (this.player == Player.WHITE? 1 : -1);
         let oneStep = new Square(piecePosition.row + playerDirection, piecePosition.col);
-        if (typeof board.getPiece(oneStep) !== 'undefined')
+        if (!oneStep.checkInRange() || typeof board.getPiece(oneStep) !== 'undefined')
             return new Array(0);
         possibleMoves.push(oneStep);
         if (!this.moved) {
             let twoStep = new Square(piecePosition.row + 2 * playerDirection, piecePosition.col);
-            if (typeof board.getPiece(twoStep) === 'undefined')
+            if (!twoStep.checkInRange() || typeof board.getPiece(twoStep) === 'undefined')
                 possibleMoves.push(twoStep);
         }
         return possibleMoves;
