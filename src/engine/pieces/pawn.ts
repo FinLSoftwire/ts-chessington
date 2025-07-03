e9ebb922bbd608b998a89fcc8a43fa281e9e6cfd
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 09:48:15 +0100

    Passing Test Red-2 (White Pawn can only move two on their first move)

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -4,8 +4,15 @@ import Board from '../board';
 import Square from '../square';
 
 export default class Pawn extends Piece {
+    private moved: boolean = false;
     public constructor(player: Player) {
         super(player);
+        this.moved = false;
+    }
+
+    public moveTo(board: Board, newSquare: Square) {
+        super.moveTo(board, newSquare);
+        this.moved = true;
     }
 
     public getAvailableMoves(board: Board) {
@@ -14,6 +21,9 @@ export default class Pawn extends Piece {
         if (this.player == Player.WHITE) {
             // Row number can increase
             possibleMoves.push(new Square(piecePosition.row + 1, piecePosition.col));
+            if (!this.moved) {
+                possibleMoves.push(new Square(piecePosition.row + 2, piecePosition.col));
+            }
         } else {
             // Row number can decrease
             possibleMoves.push(new Square(piecePosition.row - 1, piecePosition.col));
