37f6baab71c99ff3df08db92428bc3abda6aa429
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-03 10:26:05 +0100

    Passed Test Red-6 (Knight jumps in an L-shape only)

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,13 +1,30 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
+import GameSettings from "../gameSettings";
 
 export default class Knight extends Piece {
+    private DELTAS: number[][] = [];
     public constructor(player: Player) {
         super(player);
+        for (let i = -2; i < 3; i += 4) {
+            for (let j = -1; j < 2; j += 2) {
+                this.DELTAS.push([i,j]);
+                this.DELTAS.push([j,i]);
+            }
+        }
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let possibleMoves: Square[] = [];
+        let piecePosition: Square = board.findPiece(this);
+        this.DELTAS.forEach(function (value) {
+            let newPos = new Square(piecePosition.row + value[0], piecePosition.col + value[1]);
+            if (newPos.row >= 0 && newPos.row < GameSettings.BOARD_SIZE && newPos.col >= 0 && newPos.col < GameSettings.BOARD_SIZE) {
+                possibleMoves.push(newPos);
+            }
+        });
+        return possibleMoves;
     }
 }
