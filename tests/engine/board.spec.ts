11d0c1bb384e71eac10e2d9776eb86e7a221341b
Finlay Lumsden <Finlay.Lumsden@softwire.com>
2025-07-04 15:08:07 +0100

    King cannot move into check Red

--- tests/engine/board.spec.ts
+++ tests/engine/board.spec.ts
@@ -3,6 +3,8 @@ import Board from '../../src/engine/board';
 import Pawn from '../../src/engine/pieces/pawn';
 import Player from '../../src/engine/player';
 import Square from '../../src/engine/square';
+import King from "../../src/engine/pieces/king";
+import assert from "node:assert";
 
 describe('Board', () => {
 
@@ -36,6 +38,18 @@ describe('Board', () => {
 
             // Assert
             board.findPiece(pawn).should.eql(square); // Object equivalence: different objects, same data
+        });
+
+        it("can't move into check", () => {
+            // Arrange
+            const wKing = new King(Player.WHITE);
+            const bPawn = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(3, 4), wKing);
+            board.setPiece(Square.at(4, 4), bPawn);
+            board.movePiece(Square.at(3,4), Square.at(3,3));
+            assert.equal(board.getPiece(Square.at(3,4)),wKing);
+            assert.equal(board.getPiece(Square.at(3,3)),undefined);
+            assert.equal(board.currentPlayer, Player.WHITE);
         });
 
     });
